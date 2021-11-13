2ed3d5b5e89d8f2a71eb06f66c7311be06cd33dd
furkan-cloud <firstinfirstout2020@gmail.com>
2021-11-14 02:18:42 +0300

    nodemon added, express server created

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const fn = function () {
-  console.log('function working');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+const port = 4000;
+
+app.listen(port, () => {
+  console.log(`Server started at ${port}`);
+});
