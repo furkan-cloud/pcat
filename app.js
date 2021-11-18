c859918653e3271c209a2ed747da49aeecc32fa8
furkan-cloud <firstinfirstout2020@gmail.com>
2021-11-18 23:40:29 +0300

    ejs template engine added and static files updated, partials added, minor updates

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
+
+// önceden tanımlı konfigürsayon değişkeneklri kullanımı için
+//TEMPLATE ENGINE
+// view klasörüne bakar
+app.set('view engine', 'ejs');
 
 const myLogger = (req, res, next) => {
   console.log('middleware');
@@ -13,10 +19,19 @@ const myLogger = (req, res, next) => {
 app.use(express.static('public'));
 
 app.use(myLogger);
-
+// ROUTES
 app.get('/', (req, res) => {
   // res.send('hello world'); // middleware
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 const port = 4000;
