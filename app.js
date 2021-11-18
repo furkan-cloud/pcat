043d145556be1407fc6e1146fca2f7e20ef2d7f4
furkan-cloud <firstinfirstout2020@gmail.com>
2021-11-18 23:14:45 +0300

    static files uploaded, middleware added

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+const myLogger = (req, res, next) => {
+  console.log('middleware');
+  next();
+};
+
+// express static middleware
+// ekstra fonk yazdırma, statik dosya çağırma, objelerle işlem yapma
+app.use(express.static('public'));
+
+app.use(myLogger);
+
 app.get('/', (req, res) => {
-  res.send('hello world');
+  // res.send('hello world'); // middleware
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 const port = 4000;
