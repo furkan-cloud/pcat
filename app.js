3fd098570c89fdcd458bf1cbba9eb9a8f62d8421
furkan-cloud <firstinfirstout2020@gmail.com>
2021-12-01 00:58:26 +0300

    update photo feature added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
 const path = require('path');
 const fs = require('fs');
@@ -20,7 +21,7 @@ mongoose.connect('mongodb://localhost/pcat-test-db', {
 app.set('view engine', 'ejs');
 
 const myLogger = (req, res, next) => {
-  console.log('middleware');
+  // console.log('middleware');
   next();
 };
 
@@ -31,6 +32,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride('_method'));
 
 app.use(myLogger);
 // ROUTES
@@ -53,6 +55,19 @@ app.get('/photos/:id', async (req, res) => {
 
 app.get('/add', (req, res) => {
   res.render('add');
+});
+
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', { photo });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+  res.redirect(`/photos/${req.params.id}`);
 });
 
 app.post('/photos', async (req, res) => {
