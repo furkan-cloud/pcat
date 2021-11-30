976fc8a69509c07949e9eb6027d36e13b7e8b95f
furkan-cloud <firstinfirstout2020@gmail.com>
2021-12-01 01:47:46 +0300

    controllers created

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const ejs = require('ejs');
 const path = require('path');
 const fs = require('fs');
 const Photo = require('./models/Photo');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageControllers');
 
 const app = express();
 
@@ -40,70 +42,17 @@ app.use(
 );
 
 app.use(myLogger);
+
 // ROUTES
-app.get('/', async (req, res) => {
-  // res.send('hello world'); // middleware
-  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
 
-  const photos = await Photo.find({}).sort('-dateCreated');
-  res.render('index', { photos });
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/photos/:id', async (req, res) => {
-  const photo = await Photo.findById(req.params.id);
-  res.render('photo', { photo });
-});
-
-app.get('/add', (req, res) => {
-  res.render('add');
-});
-
-app.get('/photos/edit/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  res.render('edit', { photo });
-});
-
-app.put('/photos/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  photo.title = req.body.title;
-  photo.description = req.body.description;
-  photo.save();
-  res.redirect(`/photos/${req.params.id}`);
-});
-
-app.delete('/photos/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  let deletedImage = __dirname + '/public' + photo.image;
-  fs.unlinkSync(deletedImage);
-  await Photo.findByIdAndRemove({ _id: req.params.id });
-  res.redirect('/');
-});
-
-app.post('/photos', async (req, res) => {
-  // console.log(req.files.image);
-  // console.log('req', req.body);
-
-  // await Photo.create(req.body);
-  // res.render('add');
-  // res.redirect('/');
-
-  const uploadDir = 'public/uploads';
-
-  if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-  }
-
-  let uploadImage = req.files.image;
-  let uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
-  uploadImage.mv(uploadPath, async () => {
-    await Photo.create({ ...req.body, image: '/uploads/' + uploadImage.name });
-    res.redirect('/');
-  });
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
 const port = 4000;
 
